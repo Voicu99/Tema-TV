fa443c2c2fb0c7739ee7894ef9c2104789f038ff
Voicu Librimir <voicu.librimir@gmail.com>
2024-04-23 00:58:01 +0300

    rescrisa tema ca sa nu citeasca numere negative

--- Tema TV/Tema TV.cpp
+++ Tema TV/Tema TV.cpp
@@ -1,4 +1,4 @@
-// Tema TV.cpp : This file contains the 'main' function. Program execution begins and ends there.
+﻿// Tema TV.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
@@ -9,15 +9,23 @@ private:
     int width;
 
 public:
-    // Constructorul care initializeaza membrii clasei
-    TV(int h, int w) : height(h), width(w) {}
+    // Constructor care initializează membrii clasei, asigurând că înălțimea și lățimea sunt pozitive
+    TV(int h, int w) {
+        // Verifică și asigură că înălțimea și lățimea sunt pozitive
+        if (h < 0 || w < 0) {
+            std::cerr << "Eroare: Dimensiunile TV-ului trebuie sa fie pozitive.\n";
+            exit(EXIT_FAILURE); // Ieșirea din program în cazul în care dimensiunile sunt negative
+        }
+        height = h;
+        width = w;
+    }
 
     // Metoda pentru calcularea ariei
     int area() {
         return height * width;
     }
 
-    // Metode pentru accesul la inaltime si latime
+    // Metode pentru accesul la înălțime și lățime
     int getHeight() {
         return height;
     }
@@ -25,14 +33,18 @@ public:
     int getWidth() {
         return width;
     }
+
+    TV() = default;
 };
 
 int main() {
     // Exemplu de utilizare
-    TV myTV(20, 24);
+    TV myTV(20, 24); // Această linie va funcționa normal pentru dimensiunile pozitive
+    // TV myTV(-20, 24); // Această linie va genera o eroare în cazul introducerii unor dimensiuni negative
     std::cout << "Inaltimea TV-ului: " << myTV.getHeight() << " inch\n";
     std::cout << "Latimea TV-ului: " << myTV.getWidth() << " inch\n";
     std::cout << "Aria TV-ului: " << myTV.area() << " square inches\n";
 
     return 0;
 }
+
